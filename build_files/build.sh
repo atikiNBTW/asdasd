c11da3bffbd823e37256cb1abf9212cc000f0bb8
atikiNBTW <robinmusy12@gmail.com>
2025-10-18 22:58:48 +0500

    updaud

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,24 +6,16 @@ flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/fla
 systemctl disable flatpak-add-fedora-repos.service
 
 dnf5 -y copr enable bieszczaders/kernel-cachyos-lto
-dnf5 -y install kernel-cachyos-lto kernel-cachyos-lto-devel-matched
-dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
-
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
 dnf5 -y swap zram-generator-defaults cachyos-settings
 
-dnf5 -y install scx-manager
-# systemctl enable scx.service
+dnf5 -y install scx-manager git stow foot zsh neovim distrobox kernel-cachyos-lto kernel-cachyos-lto-devel-matched
+dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 
 KERNEL_SUFFIX="cachyos-lto"
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
 /usr/bin/dracut -f -p --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
 chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
-
-# touch /etc/default/scx
-# echo scx_lavd > /etc/default/scx
-
-dnf5 -y install git stow foot zsh neovim distrobox
 
 dnf5 -y copr disable bieszczaders/kernel-cachyos-lto
 dnf5 -y copr disable bieszczaders/kernel-cachyos-addons
